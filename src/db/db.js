123274f48b46f3711cb9dabc18daef9cffad515a
Gustavo Silva <gsnavarro2@gmail.com>
2022-07-09 19:46:16 -0500

    app de comunicacion en tiempo real con sockets debod agregar base de datos sql

---
+++ src/db/db.js
@@ -0,0 +1,20 @@
+//CONECTION TO DB
+import knex from 'knex';
+
+//ENV VAR
+const { MDB_CONNECTION_HOST, MDB_CONNECTION_USER, MDB_CONNECTION_PASSWORD, MDB_CONNECTION_NAME_DB } = process.env;
+
+const config = {
+    client: 'mysql',
+    connection: {
+      host: MDB_CONNECTION_HOST,
+      user: MDB_CONNECTION_USER,
+      password: MDB_CONNECTION_PASSWORD,
+      database: MDB_CONNECTION_NAME_DB
+    },
+    pool: { min: 0, max: 7 }
+};
+
+const connectionDB = knex(config);
+
+export default connectionDB;
